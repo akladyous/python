05ac712ded4136413ce08221302a59ab7016e1c5
Boula Akladyous <akladyous@gmail.com>
2023-06-02 14:06:03 -0400

    Untracked files on python

---
+++ typing-mypy/typing_demo.py
@@ -0,0 +1,44 @@
+from typing import NewType, Type
+from enum import Enum
+from mypy import checker
+
+
+T = NewType("T", str)
+user: T
+user = "John"
+
+T = NewType("T", str)
+user: T
+user = "John"  # No error, 'user' is assigned a string value
+
+# Uncomment the line below to see MyPy error
+# user = 42  # Raises a MyPy error: "Incompatible types in assignment"
+
+
+class Weekday(Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+
+class User:
+    def __init__(self, name: str, age: int, start_day: Weekday) -> None:
+        self.name = name
+        self.age = age
+
+
+u1 = User("a", 20, "Monday")
+
+
+def user_info(user: User):
+    print(user.name)
+
+
+user_info(u1)
+
+monday = Weekday.MONDAY
+print(Weekday(1))
