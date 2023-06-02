473d24b727bd5fdb1bedf568c9c7d5ded16cd94e
Boula Akladyous <akladyous@gmail.com>
2023-06-02 00:00:21 -0400

    Untracked files on python

--- orm/app.py
+++ orm/app.py
@@ -1,9 +1,18 @@
+# from typing import NewType
+
+# T = NewType("T", str)
+# user: T
+# user = "John"
+
+
 from typing import NewType
+from mypy import checker
 
 T = NewType("T", str)
-
 user: T
+user = "John"  # No error, 'user' is assigned a string value
 
-# user = "John Doe"
-user = 23223
-# print(user.upper())
+# Uncomment the line below to see MyPy error
+# user = 42  # Raises a MyPy error: "Incompatible types in assignment"
+
+checker.check_file("<input>", globals())
